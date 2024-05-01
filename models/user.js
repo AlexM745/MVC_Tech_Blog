baec60e57fcc3643d6070ade63a41c5a5d8e47f6
Alexis Merino <alexismerino49@gmail.com>
2024-05-01 18:33:43 -0500

    added the username column in the User table

--- models/user.js
+++ models/user.js
@@ -1,4 +1,14 @@
-const {Model, Datatypes} = require ("sequelize");
+// importing sequelize connection, model and dataypes from sequelize 
+const { Model, Datatypes } = require("sequelize");
 const sequelize = require('../config/connection');
+// the user is the superclass and the model the subclass that is inheriting.
+class User extends Model { }
 
-class User extends Model {}
+User.init({
+    // adding the column for user
+    username: {
+        type: Datatypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+})
