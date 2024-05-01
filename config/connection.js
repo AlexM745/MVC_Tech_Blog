86ac3fb513eda5873ce1519d08bfecdff0bbe1e5
Alexis Merino <alexismerino49@gmail.com>
2024-04-30 19:37:02 -0500

    finished the connection.js with comments and added .env file

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,24 @@
+// importing squelize and dotenv
 const Sequelize = require("sequelize");
 require('dotenv').config();
+// squelize is an empty variable 
+let sequelize;
+
+if (process.env.DB_URL) {
+    // seuqlize is now processing the url from the data base to establish connection
+    sequelize = new Sequelize(process.env.DB_URL);
+} else{
+    // using imported user enviorment varibles from .env to connect with database
+    sequelize = new Sequelize(
+        process.env.DB_NAME, 
+        process.env.DB_USER, 
+        process.env.DB_PASSWORD, 
+        // the host of the url and it will use postgres 
+        {
+            host: "localhost", 
+            dialect: "postgres", 
+        }
+    );
+}
+// export sequelize for use in the server.js
+module.exports = sequelize;
