24be94556db4486f866cddfd391bdf2c7b2b716f
Alexis Merino <alexismerino49@gmail.com>
2024-05-02 22:09:43 -0500

    added the title column to blog table

--- models/blog.js
+++ models/blog.js
@@ -1,5 +1,13 @@
 // importing the model and datatypes 
 const {Model, DataTypes } = require ("sequelize");
 const sequelize = require("../config/connection");
+// sub class is blod that inherits from model the super class
+class Blog extends Model {}
 
-
+Blog.init({
+    // added the title column
+    title: {
+        type: DataTypes.STRING, 
+        allowNull:false;
+    }
+})
