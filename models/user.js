2d2c652e4a95229d3cf15e694c42db724cea0131
Alexis Merino <alexismerino49@gmail.com>
2024-05-02 23:08:16 -0500

    added a hook and sequelize model properties

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 // importing sequelize connection, model and dataypes from sequelize 
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require('../config/connection');
+// imports bcrypt to has passwords
+const bcrypt = require("bcrypt");
 // the user is the subclass that is inheriting and the model the superclass .
 class User extends Model { }
 
@@ -19,7 +21,23 @@ User.init({
             len:[8]
         }
     },
-})
+
+}, {
+    // adding a hook to trigger when the user password is added that it is hashed
+    // by using bcrypt npm package
+    hooks:{
+    beforeCreate: async userdata => {
+        userdata.password = await bcrypt.hash(userdata.password, 5)
+        return userdata
+    }
+
+    }, 
+    // model propeties
+    sequelize, 
+    freezeTableName: true, 
+    underscored: true, 
+    modelName: "user", 
+});
 
 // exports the User model
 module.exports = User;
