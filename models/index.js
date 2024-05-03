f66aacb22dedb67d65f796070a924e2328197007
Alexis Merino <alexismerino49@gmail.com>
2024-05-02 23:35:29 -0500

    : added the associations for the models so that they are connected.

--- models/index.js
+++ models/index.js
@@ -2,3 +2,21 @@
 const User = require("./user");
 const Blog = require("./blog");
 const Comment = require("./comment");
+
+// associations for the tables 
+
+User.hasMany(Blog);
+Blog.belongsTo(User);
+
+Blog.hasMany(Comment);
+Comment.belongsTo(Blog);
+
+User.hasMany(Comment);
+Comment.belongsTo(User);
+
+// export the models
+module.exports = {
+    User, 
+    Blog, 
+    Comment
+}
