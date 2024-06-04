18a25bf0440c7761a544c2ca423c38b3636bbea3
Alexis Merino <alexismerino49@gmail.com>
2024-06-04 10:32:41 -0500

    POST for new Blog post

--- controller/api/postRoutes.js
+++ controller/api/postRoutes.js
@@ -4,3 +4,16 @@ const router = require("express").Router();
 const {Blog, Comment, User} = require("../../models");
 // importing auth for help authenticating
 const withauth = require("../../util/auth");
+
+// ('/api/blog')
+//POST to create a new blog post
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.create({ ...req.body, userId: req.session.userId });
+        console.log("This is the new blog post", blogData);
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
