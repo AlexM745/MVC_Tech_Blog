9e99d75540f3be7b7b639a2cd697714293d5440e
Alexis Merino <alexismerino49@gmail.com>
2024-06-04 10:17:04 -0500

    POST login

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -22,5 +22,32 @@ router.post("/", async (req, res) => {
     }
 });
 
+//POST to check if the user information matches the data base and logs in the user.Once logged in it saves the user id and logged_in state to session
+router.post("/login", async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+        if (!userData) {
+            res.status(400).json({message:"Incorrect email or password, try again"})
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({message:"Incorrect email or password, try again"})
+            return;
+        }
+
+        req.session.save(()=>{
+            req.session.user_id = userData.id;
+            req.session.login_in = true;
+
+            res.json({user: userData, message:"You are logged in!!"})
+        });
+
+    }catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 
 
