438ff4d93f19fdcd68cf3678a111ff7109e2c121
Alexis Merino <alexismerino49@gmail.com>
2024-06-04 09:30:31 -0500

    imported dependecies and added comments

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -1,6 +1,9 @@
 // importing express and router
 const express = require("express");
+//imporiting express as a router
 const router = express.Router();
 // importing the models to use database information for routes
 const{User, Blog, Comment} = require("../../models");
+// importing bcrypt to import and hash passwords
+const bcrypt = require("bcrypt");
 
