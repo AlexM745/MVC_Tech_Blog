7c01f0aceaaf70e1652eef244f5fd8d08025925a
Alexis Merino <alexismerino49@gmail.com>
2024-06-05 11:24:32 -0500

    adjusted connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -6,17 +6,24 @@ let sequelize;
 
 if (process.env.DB_URL) {
     // seuqlize is now processing the url from the data base to establish connection
-    sequelize = new Sequelize(process.env.DB_URL);
-} else{
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
+    });
+} else {
     // using imported user enviorment varibles from .env to connect with database
     sequelize = new Sequelize(
-        process.env.DB_NAME, 
-        process.env.DB_USER, 
-        process.env.DB_PASSWORD, 
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
         // the host of the url and it will use postgres 
         {
-            host: "localhost", 
-            dialect: "postgres", 
+            host: "localhost",
+            dialect: "postgres",
         }
     );
 }
