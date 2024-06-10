0f30fc7dadba0f915eab4502e4c36e9c9101fa8a
Alexis Merino <alexismerino49@gmail.com>
2024-06-10 08:10:02 -0500

    correcting login .js and login form

--- public/js/login.js
+++ public/js/login.js
@@ -2,20 +2,20 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
 
     // Get values from the login.handlebars
-    const email = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector("#username-login").value.trim();
+    const password = document.querySelector("#password-login").value.trim();
 
-    if (email && password) {
+    if (username && password) {
         // Send a POST request to the API endpoint
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({ email, password }),
-            headers: { 'Content-Type': 'application/json' },
+        const response = await fetch("/api/users/login", {
+            method: "POST",
+            body: JSON.stringify({ username, password }),
+            headers: { "Content-Type": "application/json" },
         });
 
         if (response.ok) {
-            // If successful, redirect the browser to the profile page
-            document.location.replace('/dashboard');
+            // If successful, redirect the browser to the dashboard page
+            document.location.replace("/dashboard");
         } else {
             alert(response.statusText);
         }
@@ -26,18 +26,18 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector("#username-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
 
-    if (name && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ name, email, password }),
-            headers: { 'Content-Type': 'application/json' },
+    if (username && password) {
+        const response = await fetch("/api/users", {
+            method: "POST",
+            body: JSON.stringify({ username, password }),
+            headers: { "Content-Type": "application/json" },
         });
 
         if (response.ok) {
-            document.location.replace('/dashboard');
+            document.location.replace("/dashboard");
         } else {
             alert(response.statusText);
         }
@@ -46,9 +46,9 @@ const signupFormHandler = async (event) => {
 
 // adding event listeners to log in button and sign up button.
     document
-        .querySelector('.login-form')
-        .addEventListener('submit', loginFormHandler);
+        .querySelector(".login-form")
+        .addEventListener("submit", loginFormHandler);
 
     document
-        .querySelector('.signup-form')
-        .addEventListener('submit', signupFormHandler);
+        .querySelector(".signup-form")
+        .addEventListener("submit", signupFormHandler);
